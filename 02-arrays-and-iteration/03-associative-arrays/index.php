0c5c9c278dffd303ed2b69fb3a5c459a066188d2
Muhamad Ibnu Khaidar Hafiz <ibnukhaidar@gmail.com>
2025-09-30 00:00:53 +0700

    associative array

---
+++ 02-arrays-and-iteration/03-associative-arrays/index.php
@@ -0,0 +1,50 @@
+<?php
+$output = null;
+
+$user = [
+    'name' => 'Brad',
+    'email' => 'brad@example.com',
+    'password' => '12345',
+    'hobbies' => ['football', 'tennis', 'swimming', 'gaming']
+];
+
+$output = $user['name'];
+$output .= " <br/> $user[email]";
+$output .= " <br/> {$user['hobbies'][2]}";
+
+$user['address'] = '123 Street';
+
+unset($user['address']);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>PHP From Scratch</title>
+</head>
+
+<body class="bg-gray-100">
+    <header class="bg-blue-500 text-white p-4">
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-semibold">PHP From Scratch</h1>
+        </div>
+    </header>
+    <div class="container mx-auto p-4 mt-4">
+        <div class="bg-white rounded-lg shadow-md p-6 mt-6">
+            <!-- Output -->
+            <p class="text-xl"><?= $output ?></p>
+            <h2 class="text-xl font-semibold my-4">User Array</h2>
+            <p></p>
+                <pre>
+                    <?= print_r($user) ?>
+                </pre>
+            </p>
+        </div>
+    </div>
+</body>
+
+</html>
