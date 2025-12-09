bb622bc02e42d8df4a64b430f0dd45321d3c8dd2
Ibnu Khaidar <ibnukhaidar@gmail.com>
2025-12-09 20:59:53 +0700

    done 05/02

---
+++ 05-oop/02-access-modifiers-getters-setters/index.php
@@ -0,0 +1,48 @@
+<?php
+
+class User
+{
+  // Properties
+  public $name;
+  public $email;
+  private $status = 'active';
+
+  public function __construct($name, $email)
+  {
+    $this->name = $name;
+    $this->email = $email;
+  }
+
+  // Methods
+  public function login()
+  {
+    echo $this->name . ' logged in <br>';
+  }
+
+  // getter
+  public function getStatus() {
+    return $this->status;
+  }
+
+  // setter
+  public function setStatus($status) {
+    $this->status = $status;
+  }
+}
+
+// Instantiate a new object
+$user1 = new User('John Doe', 'john@gmail.com');
+
+$user1->login();
+
+$user2 = new User('Jane Doe', 'jane@gmail.com');
+
+$user2->login();
+
+echo $user1->setStatus('inactive');
+echo $user1->getStatus();
+
+// echo $user2->setStatus("inactive");
+// echo $user2->getStatus();
+
+// var_dump($user2);
