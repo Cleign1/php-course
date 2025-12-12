465bce0193547c0272eb2dee94b1d696e9e64d2c
Ibnu Khaidar <ibnukhaidar@gmail.com>
2025-12-12 23:22:48 +0700

    done 06/03

---
+++ 06-superglobals/03-get/index.php
@@ -0,0 +1,8 @@
+<?php
+// Add a query parameter to the URL in the browser's address bar:
+// http://localhost:8000/?name=John
+// echo $_GET['name'] ? $_GET['name'] : "";
+echo htmlspecialchars($_GET['name'] ?? '');
+
+
+?>
