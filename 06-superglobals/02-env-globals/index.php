3e1064d75b5b7ca43fe5c7906a52066e2beddd1d
Ibnu Khaidar <ibnukhaidar@gmail.com>
2025-12-12 21:38:55 +0700

    done 06/02

---
+++ 06-superglobals/02-env-globals/index.php
@@ -0,0 +1,37 @@
+<?php
+  putenv('DB_HOST=localhost');
+  putenv('DB_USER=user');
+
+  $db_host = getenv("DB_HOST");
+  $db_user = getenv('DB_USER');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>System Information</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-gray-100">
+  <div class="container mx-auto p-8 bg-white shadow-md mt-10 rounded-lg">
+    <h1 class="text-3xl font-semibold mb-4 text-center">System Information</h1>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+
+      <div class="bg-gray-200 p-4 rounded-lg">
+        <strong class="block mb-2">DB Host:</strong>
+          <?= $db_host ?>
+      </div>
+      <div class="bg-gray-200 p-4 rounded-lg">
+        <strong class="block mb-2">DB User:</strong>
+          <?= $db_user ?>
+      </div>
+
+    </div>
+</body>
+
+</html>
